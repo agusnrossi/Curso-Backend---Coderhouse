2d17632b662b365904962a9089adde4f4b8c0774
Agustin Rossi <agusrossi021287@gmail.com>
2022-03-14 13:55:36 -0300

    desafio_5_parte_ejs

---
+++ Desafio_entregable_5/ejs/main.js
@@ -0,0 +1,27 @@
+const express=require('express');
+const router = require("./routes/router");
+
+//---------------------------------------------//
+const app=express();
+const PORT=8080
+
+//-------------------------------------------//
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'))
+app.use("/", router);
+
+//-------------------------------------------------------//
+app.set('view engine','ejs')
+
+//-------------------------------------------------------------------//
+const server=app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
+
+server.on('error',(err)=>{
+    console.log(err)
+})
+
+//------------------------------------------------------------------//
+
