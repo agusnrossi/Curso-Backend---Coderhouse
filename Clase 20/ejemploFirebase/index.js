402d39e8cfc147ac150d08d1c13d28094c43cbab
Agustin Rossi <agusrossi021287@gmail.com>
2022-04-23 00:48:42 -0300

    mas commits de clase 20

--- Clase 20/ejemploFirebase/index.js
+++ Clase 20/ejemploFirebase/index.js
@@ -22,3 +22,36 @@ async function CRUD() {
         console.log(error)
     }
 }
+
+
+/* 
+
+    try {
+        const id = 3
+        const doc = query.doc(`${id}`)
+        await doc.create({ nombre: 'Juan', dni: '33403846' })
+        const queryTodos = await query.get();
+        const response = queryTodos.docs.map((doc) => ({
+
+            id: doc.id,
+            nombre: doc.data().nombre,
+            dni: doc.data().dni
+        }))
+        console.log(response)
+    }
+    catch (e) {
+        console.log(e)
+    }
+
+
+//---------------------//
+try {
+    let id = 2;
+    const doc = query.doc(`${id}`)
+    await doc.update({ dni: '40403846' })
+    console.log('actualizado')
+}
+catch (e) {
+    console.log(e)
+}
+ */
