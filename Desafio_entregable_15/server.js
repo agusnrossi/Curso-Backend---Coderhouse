b90d8d7f1301eda498cd266ad6f6411325e734df
Agustin Rossi <agusrossi021287@gmail.com>
2022-06-27 19:13:25 -0300

    desafio 15 arreglo en server

--- Desafio_entregable_15/server.js
+++ Desafio_entregable_15/server.js
@@ -98,7 +98,7 @@ if (args.MODE === 'CLUSTER' && cluster.isPrimary) {
 
     //-----------------------------------------------------//
 
-    mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect('mongodb+srv://agusnrossi:Tottenham@cluster0.yrwhz.mongodb.net/ecommerce?retryWrites=true&w=majority')
         .then(() => console.log('conexion exitosa!'))
         .catch(err => console.log(err))
 
