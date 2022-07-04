3465667b6a0adcf9ac3bcc4cf109b14335496cb0
Agustin Rossi <agusrossi021287@gmail.com>
2022-07-04 18:00:20 -0300

    arreglos definitivos

--- Tercer_entrega_Proyecto_Final/models/daos/cart/cartDaoFirebase.js
+++ Tercer_entrega_Proyecto_Final/models/daos/cart/cartDaoFirebase.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const ContenedorFirebase = require('../../contenedores/contenedorFirebase');
-=======
-const FirebaseContainer= require('../../../container/firebaseContainer');
->>>>>>> 8031afd8eff374b5f54386c1c8c5b71091384847
 
 class CartDaoFirebase extends ContenedorFirebase {
   constructor() {
--- Tercer_entrega_Proyecto_Final/models/daos/products/productDaoFirebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseContainer = require('../../../container/firebaseContainer');
-
-class prductDaoFirebase extends firebaseContainer {
-    constructor() {
-        super('products');
-    }
-}
-
-module.exports = prductDaoFirebase;
