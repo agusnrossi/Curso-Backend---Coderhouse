cbb6e9aba1ecb6bb5a951780924c9ceb22d4588a
Agustin Rossi <agusrossi021287@gmail.com>
2022-07-28 19:22:31 -0300

    arreglos desafio 19

--- Desafio_entregable_19/src/models/daos/graphQL/products.graphql.schema.js
+++ Desafio_entregable_19/src/models/daos/graphQL/products.graphql.schema.js
@@ -1,34 +1,32 @@
 const productSchema = `
-    type Product {
-        id: ID!,
-        code: String,
-        name: String,
-        desc: String,
-        image: String,
-        price: Int,
-        stock: Int,
-        timestamp: String
-    }
-
-    input newInfoProduct {
-        name: String,
-        desc: String,
-        image: String,
-        price: Int,
-        stock: Int
-    }
-
-
-    type: Query {
-        getAllProducts: [Product],
-        getProductById(id: ID!): Product,
-    }
-
-    type: Mutation {
-        saveNewProduct(info: newInfoProduct): Product,
-        updateProduct(id: ID!, info: newInfoProduct): Product,
-        deleteProduct(id: ID!): Product,
-    }
-`;
+type Product {
+    id: ID!,
+    code: String,
+    name: String,
+    desc: String,
+    image: String,
+    price: Int,
+    stock: Int,
+    timestamp: String,
+  }
+  input newInfoProduct {
+    name: String,
+    desc: String,
+    image: String,
+    price: Int,
+    stock: Int,
+  }
+  type Query {
+    getAllProducts: [Product],
+    getProductById(id: ID!): Product,
+  }
+  type Mutation {
+    saveNewProduct(datos: newInfoProduct): Product,
+    updateProdcut(id: ID!, datos: newInfoProduct): Product,
+    deleteProduct(id:ID!): Product,
+  }
+`
 
 module.exports = productSchema;
+
+
